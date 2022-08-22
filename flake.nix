e513282f93d01d7560e06f4c32e2df68f98fa16c
Hofsiedge <hofsiedge@gmail.com>
2022-08-22 22:05:04 +0900

    first attempt...

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  inputs.nixpkgs.url = "nixos/nixpkgs-unstable";
+  inputs.home-manager.url = "github:nix-community/home-manager";
+  
+  outputs = { self, nixpkgs, ... }@attrs: {
+    nixosConfigurations.hofsiedge = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = attrs;
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
